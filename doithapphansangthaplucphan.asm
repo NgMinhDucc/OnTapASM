01e8d3bff390fa93ee8f39309a014801da734606
NgMinhDucc <ducnm@gdscptit.dev>
2026-06-23 01:25:24 +0700

    June 23 2026

--- doithapphansangthaplucphan.asm
+++ doithapphansangthaplucphan.asm
@@ -1,66 +1,91 @@
 .model small
 .stack 100h
 .data
-crlf db 13, 10, '$'
 x dw 0
-y dw ?
-muoi dw 10
-hex dw 16
+crlf db 13, 10, '$'
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
     
+    mov bx, 10
     nhap:
-        mov ah, 1
-        int 21h
-        
-        cmp al, 13
-        je khoitao
-        
-        mov ah, 0
-        sub al, 48
-        
-        mov y, ax
-        mov ax, x
-        mul muoi
-        add ax, y
-        mov x, ax
-        jmp nhap
+    mov ah, 1
+    int 21h
+    
+    cmp al, 13
+    je khoitao
+    
+    mov ah, 0
+    sub al, 48
+    
+    mov cx, ax
+    mov ax, x
+    mul bx
+    add ax, cx
+    mov x, ax
+    jmp nhap
     
     khoitao:
-        lea dx, crlf
-        mov ah, 9
-        int 21h
-        
-        mov ax, x
-        mov cx, 0
+    lea dx, crlf
+    mov ah, 9
+    int 21h
     
-    chia:
-        mov dx, 0
-        div hex
-        push dx
-        inc cx
-        
-        cmp ax, 0
-        je quydoi
-        jmp chia
+    mov ax, x
+    mov bx, 2
+    mov cx, 0
+    
+    chia2:
+    mov dx, 0
+    div bx
+    push dx
+    inc cx
+    
+    cmp ax, 0
+    je inra2
+    jmp chia2
+    
+    inra2:  
+    pop dx
+    add dl, 48
+    mov ah, 2
+    int 21h   
+    loop inra2
+    
+    khoitao2:
+    lea dx, crlf
+    mov ah, 9
+    int 21h
+    
+    mov ax, x
+    mov bx, 16
+    mov cx, 0
+    
+    chia16:
+    mov dx, 0
+    div bx
+    push dx
+    inc cx
+    
+    cmp ax, 0
+    je quydoi
+    jmp chia16
     
     quydoi:
-        pop dx
-        cmp dl, 9
-        ja phanchu
-        add dl, 48
-        jmp inra
+    pop dx
+    cmp dl, 9
+    ja phanchu
+    add dl, 48
+    jmp inra16
     
     phanchu:
-        add dl, 55
+    add dl, 55
     
-    inra:
-        mov ah, 2
-        int 21h
-        loop quydoi
+    inra16:
+    mov ah, 2
+    int 21h
+    loop quydoi
     
     mov ah, 4ch
     int 21h
